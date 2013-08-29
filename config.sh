a46691b0cbe0b35377a59b2a1c14e5158adb20f5
Kaleb Elwert <kelwert@mtu.edu>
2013-08-29 18:14:03 -0400

    Small setup changes

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ## Belak's setup.sh
 
+## NOTE
+# Even though this sets up config files, .gitconfig and .hgrc are in setup.sh
+
 ## TODO
 # Make all user input only needed at the start
 # MPD autostart on OSX
@@ -43,31 +46,3 @@ config_link Xdefaults
 config_link belak
 
 vim +BundleInstall +qall
-
-[[ ! -f "$HOME/.ssh/id_rsa.pub" ]] && ssh-keygen
-
-if [[ ! -f ~/.hgrc || ! -f ~/.gitconfig ]]
-then
-	## VCS setup
-	read -p "VCS User: " vcs_user
-	read -p "VCS Email: " vcs_email
-
-	if [[ ! -f ~/.hgrc ]]
-	then
-		read -p "Bitbucket Username: " bb_user
-		read -s -p "Bitbucket Password: " bb_pass
-		curl -u "$bb_user:$bb_pass" --data-urlencode "key=$(<$HOME/.ssh/id_rsa.pub)" --request POST https://api.bitbucket.org/1.0/ssh-keys/
-		[[ ! -f "$HOME/.hgrc" ]] && printf '[ui]\nusername = %s <%s>\n' "$vcs_user" "$vcs_email" > $HOME/.hgrc
-	fi
-
-	if [[ ! -f ~/.gitconfig ]]
-	then
-		read -p "Github Username: " gh_user
-		read -s -p "Github Password: " gh_pass
-		echo "{}" | jshon -s "$(hostname)" -i "title" -s "$(<$HOME/.ssh/id_rsa.pub)" -i "key" | curl -u "$gh_user:$gh_pass" --data @- https://api.github.com/user/keys
-		git config --global user.name "$vcs_user"
-		git config --global user.email "$vcs_email"
-		git config --global push.default simple
-		git config --global branch.autorebase always
-	fi
-fi
