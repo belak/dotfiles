736e1b4267754ddd5a4d59802cad11e06a935d99
Kaleb Elwert <belak@coded.io>
2013-09-21 02:45:38 -0400

    fixed a typo

--- setup.sh
+++ setup.sh
@@ -68,7 +68,7 @@ then
 	if [[ -n $BELAK_ARCH && ! -f .arch-init ]]
 	then
 		mkdir ~/docs/aur
-		sudo pacman -S stow aweome htop zsh git mercurial base-devel boost xdg-user-dirs
+		sudo pacman -S stow awesome htop zsh git mercurial base-devel boost xdg-user-dirs \
 			alsa-utils cmake faience-icon-theme lxappearance mlocate openssh \
 			python-virtualenvwrapper python-pip python2-pip \
 			rxvt-unicode xorg-server xorg-xinit xorg-xrandr wget terminus-font jshon
