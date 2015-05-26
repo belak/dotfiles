dfa3899fbead417b7270ce29ba7c81baef3ce9b1
Kaleb Elwert <belak@coded.io>
2015-05-26 01:40:07 -0700

    Large emacs update

--- emacs.d/lisp/init-osx.el
+++
@@ -1,9 +0,0 @@
-(when (equal system-type 'darwin)
-  ;; Treat option as meta and command as super
-  (setq mac-option-key-is-meta t)
-  (setq mac-command-key-is-meta nil)
-  (setq mac-command-modifier 'super)
-  (setq mac-option-modifier 'meta)
-  (set-frame-font "Monaco 12"))
-
-(provide 'init-osx)
--- emacs.d/lisp/init-settings.el
+++
@@ -1,9 +0,0 @@
-;; Better defaults
-(require-package 'better-defaults)
-
-;; Random settings
-(add-hook 'prog-mode-hook 'linum-mode)
-(setq initial-buffer-choice t)
-(setq inhibit-startup-screen t)
-
-(provide 'init-settings)
