c204143c7782ba8ce8cbac9cc4ca6bd870960f07
Kaleb Elwert <belak@coded.io>
2015-07-26 22:28:19 -0700

    emacs: refactor and new stuff
    There's actually quite a bit in this commit, so I'll do my best to break
    it down here.
    
    - Move almost everything out of init.el
      The org package should exist in every "modern" emacs install so we
      shouldn't worry about it at all.
    - Finalization section was cleaned up so we don't copy some of the hooks
      from elsewhere in the file.
    - Enable debugging while starting emacs so it's easier to debug
    - Enable auto-fill on modes derived from text-mode
    - Update js handling with cleaner syntax error highlights, as well as
      better completion and other stuffs (or so I've heard) using tern.
    - Make lambda prettier in elisp
    - Remove a few extra after-init-hook calls because use-package makes
      those superflous.
    - Clean up some VC handling
    - Add back the normal isearch as C-s
    - Add a bind for magit-status

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,36 +1,3 @@
-;;; init.el - Config bootstrap
-;; A few things need to happen in this file:
-;; - Load package system
-;; - Install org-mode
-;; - Bootstrap into README.org
-
-;; Load package system
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives
-      '(("gnu"       . "http://elpa.gnu.org/packages/")
-        ("marmalade" . "https://marmalade-repo.org/packages/")
-        ("melpa"     . "http://melpa.org/packages/")
-        ("elpy"      . "http://jorgenschaefer.github.io/packages/")))
-
-;; Make sure it's initialized
-(package-initialize)
-
-;; Update package lists if we don't have any yet
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; Small macro to install a missing package
-(defun package-ensure-installed (package)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-;; Install org-mode
-(package-ensure-installed 'org)
-
-;; Load org-mode
 (require 'org)
-(require 'ob-tangle)
-
-;; Bootstrap into README.org
-(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+(org-babel-load-file
+ (expand-file-name "README.org" user-emacs-directory))
