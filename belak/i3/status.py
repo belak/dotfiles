2e9d5d34a115407c7a59e0ad495589220a188fef
Kaleb Elwert <belak@coded.io>
2015-07-28 04:06:34 -0700

    i3: new status bar

---
+++ belak/i3/status.py
@@ -0,0 +1,31 @@
+from i3pystatus import Status
+from i3pystatus.updates import pacman, cower
+
+status = Status(standalone=True, click_events=False)
+
+status.register("clock", format="  %d-%m-%Y %H:%M", interval=20)
+
+status.register("network",
+                interface="wlp3s0",
+                format_up="  {essid}",
+                format_down="  none")
+
+status.register("battery",
+                format="{status} {percentage:.0f} {remaining}",
+                status={
+                    'DPL': '',
+                    'FULL': 'FULL',
+                    'CHR': 'CHR',
+                    'DIS': ''
+                })
+
+status.register("alsa",
+                format="  {volume}",
+                format_muted="  {volume}")
+
+status.register("updates",
+                format="{count}",
+                format_no_updates="No updates",
+                backends=[pacman.Pacman(), cower.Cower()])
+
+status.run()
