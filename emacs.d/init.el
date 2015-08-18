f49fad68d35634e471b8d0f60672617bc6ce3dec
Kaleb Elwert <belak@coded.io>
2015-08-18 11:59:30 -0400

    emacs: move back to use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,16 @@
+(defconst emacs-start-time (current-time))
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "README.org" user-emacs-directory))
+
+(when window-system
+  (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
+    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
+
+  (add-hook 'after-init-hook
+            `(lambda ()
+               (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
+                 (message "Loading %s...done (%.3fs) [after-init]"
+                          ,load-file-name elapsed)))
+            t))
