f2f66f3674c20c22f775c30af2bc1117db25f4ad
Kaleb Elwert <belak@coded.io>
2015-09-13 12:13:18 -0700

    emacs: fix up evil-mode colors

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,20 @@
+;; init -- belak's emacs init.el
+;;
+;;; Commentary:
+;; This file is only to bootstrap into README.org and set up some
+;; basic timing.
+;;
+;;; Code:
+
+;; Define the start time so we can measure how long loading took later.
 (defconst emacs-start-time (current-time))
 
+;; Load the org-mode file. This has everything aside from the timing.
 (require 'org)
 (org-babel-load-file
  (expand-file-name "README.org" user-emacs-directory))
 
+;; Now that we're done loading everything, print how long it took.
 (when window-system
   (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
     (message "Loading %s...done (%.3fs)" load-file-name elapsed))
@@ -14,3 +25,5 @@
                  (message "Loading %s...done (%.3fs) [after-init]"
                           ,load-file-name elapsed)))
             t))
+
+;;; init.el ends here
