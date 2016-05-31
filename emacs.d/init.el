8b38b21e388cc062a1c70da0252470e501667ff5
Kaleb Elwert <belak@coded.io>
2016-05-31 03:24:02 -0700

    emacs: reorganize into an even simpler solution

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,10 +21,7 @@
       debug-on-quit t)
 
 ;; Load in our extra stuff
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/"))
-
-;; Load all the libraries and tools we need.
-(require 'init-frame-hooks)
+;;(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/"))
 
 ;;;; Platform-Specific
 
@@ -112,18 +109,274 @@
 
 (setq use-package-always-ensure t)
 
-;;;; Appearance
+;;;; Early Loading
+;; Portions of my init.el depend on other components. The most
+;; important of which are loaded here.
+
+;; Load all important custom libraries
+(use-package init-frame-hooks
+  :ensure nil
+  :load-path "lisp/")
+
+;; company-mode is used as a completion system. In use-package blocks,
+;; you can use :if (fboundp 'company-mode) to only enable a block if
+;; company-mode is active.
+(use-package company
+  :diminish company-mode
+  :config
+  (setq company-idle-delay 0
+	company-echo-delay 0
+	company-minimum-prefix-length 1)
+  (global-company-mode))
+
+;; flycheck-mode is used for linters and catching compilation
+;; errors. In use-package blocks, you can use :if (fboundp
+;; 'flycheck-mode) to only enable a block if flycheck-mode is enabled.
+(use-package flycheck
+  :diminish flycheck-mode
+  :config
+  (defalias 'flycheck-show-error-at-point-soon 'flycheck-show-error-at-point)
+  (global-flycheck-mode))
+
+;; Project based navigation is pretty much the best thing ever.
+(use-package projectile
+  :diminish projectile-mode
+  :config
+  (projectile-global-mode))
 
 (use-package zenburn-theme
   :config
   (load-theme 'zenburn t))
 
+;;;; Packages
+;; Now that all the important packages have been loaded, we load
+;; everything else in alphabetical order.
+
+;; anaconda mode provides code navigation and docs. Additionally, if
+;; company-mode is enabled, company-anaconda will also be
+;; enabled. TODO: Right now this mode is sort of hard to remember. Maybe it
+;; would make sense to bundle a bunch of python things under
+;; python-mode.
+(use-package anaconda-mode
+  :diminish anaconda-mode
+  :config
+  (use-package company-anaconda
+    :if (fboundp 'company-mode)
+    :config (add-to-list 'company-backends 'company-anaconda))
+
+  (when (fboundp 'projectile-mode)
+    (defun belak/virtualenv-magic (&rest arg-list)
+      (if (and (projectile-project-p) (venv-is-valid (projectile-project-name)))
+	  (venv-workon (projectile-project-name))))
+    (advice-add 'switch-to-buffer :after #'belak/virtualenv-magic))
+
+  (add-hook 'python-mode-hook 'anaconda-mode))
+
+;; anzu shows how many matches in isearch.
+(use-package anzu
+  :diminish anzu-mode
+  :config (global-anzu-mode))
+
+;; diff-hl uses the emacs vcs integration to display
+;; added/modified/removed lines.
+(use-package diff-hl
+  :config
+  (add-hook 'after-make-console-frame-hooks
+	    (lambda ()
+	      (global-diff-hl-mode 0)
+	      (diff-hl-margin-mode 1)))
+  (add-hook 'after-make-window-system-frame-hooks
+	    (lambda ()
+	      (global-diff-hl-mode 1)
+	      (diff-hl-margin-mode 0))))
+
+;; editorconfig is a simple way to share indentation settings between
+;; editors. Because I sometimes dabble in neovim and sublime, it's
+;; nice to not have to re-do these settings at a project level between
+;; editors.
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+;; Grab important environment variables from the shell. The important
+;; ones are PATH and GOPATH.
+(use-package exec-path-from-shell
+  :config
+  (add-to-list 'exec-path-from-shell-variables "GOPATH")
+  (exec-path-from-shell-initialize))
+
+;; fic-mode simply gives an annoying highlight to FIXME, TODO, XXX,
+;; and other similar keywords so they're easy to spot.
+(use-package fic-mode
+  :diminish fic-mode
+  :config
+  (add-to-list 'fic-highlighted-words "XXX")
+  (add-hook 'prog-mode-hook 'fic-mode))
+
+;; flyspell does what it says on the tin. It's a spell-checker similar to flycheck.
+(use-package flyspell
+  :diminish flyspell-mode
+  :config (add-hook 'text-mode-hook (lambda () (flyspell-mode 1))))
+
+(use-package go-mode
+  :mode "\\.go\\'"
+  :config
+  (use-package company-go
+    :if (fboundp 'company-mode)
+    :config
+    (add-to-list 'company-backends 'company-go))
+
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq gofmt-command "goimports"))
+
+;; ido (interactively-do) is a better interface for selecting things.
+(use-package ido
+  :config
+  ;; smex is a better replacement for M-x built around ido.
+  (use-package smex
+    :bind
+    ("M-x" . smex)
+    ("M-X" . smex-major-mode-commands))
+
+  ;; Use ido everywhere possible.
+  (use-package ido-ubiquitous
+    :config
+    (ido-ubiquitous-mode 1))
+
+  ;; ido is much more readable when all the options are displayed
+  ;; vertically.
+  (use-package ido-vertical-mode
+    :config
+    (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right
+	  ido-vertical-show-count t)
+    (ido-vertical-mode 1))
+
+  ;; flx-ido changes the matching algorithm to improve the flex
+  ;; matching support.
+  (use-package flx-ido
+    :config
+    (setq ido-enable-flex-matching t
+	  flx-ido-threshold 1000))
+
+  (setq resize-mini-windows t
+	ido-use-virtual-buffers t
+	ido-auto-merge-work-directories-length -1)
+
+  (ido-mode 1)
+  (ido-everywhere 1))
+
+;; js2-mode is a wrapper around js-mode which cleans it up and adds a
+;; bunch of features.
+(use-package js2-mode
+  :mode "\\.js\\'"
+  :config
+  (setq js2-basic-offset 2)
+  (when (fboundp 'flycheck-mode)
+    (set-face-attributes 'js2-error nil
+			 :inherit 'flycheck-error-list-error
+			 :underline '(:color foreground-color :style wave))
+    (set-face-attributes 'js2-warning nil
+			 :inherit 'flycheck-error-list-warning
+			 :underline '(:color foreground-color :style wave))))
+
+(use-package less-css-mode
+  :mode "\\.less\\'")
+
+;; magit is an amazing tool for working with git inside emacs.
+(use-package magit
+  :bind ("M-g M-g" . magit-status)
+  :init
+  (use-package magit-filenotify
+    :if (linux-p)
+    :config
+    (add-hook 'magit-status-mode-hook 'magit-filenotify-mode))
+  :config
+  (setq magit-push-always-verify t
+	magit-completing-read-function 'magit-ido-completing-read))
+
+(use-package pip-requirements
+  :mode
+  "requirements.txt"
+  "requirements/\\.txt\\'")
+
+(use-package rainbow-mode
+  :commands rainbow-mode)
+
+;; recentf adds some useful functionality to ido which remembers
+;; previously opened files.
+(use-package recentf
+  :ensure nil
+  :config
+  (recentf-mode 1))
+
+;; Save the last location when you leave a file.
+(use-package saveplace
+  :ensure nil
+  :config
+  (setq-default save-place t))
+
+;; smart-mode-line is a package which aims to provide a better
+;; mode-line with little configuration. I've tried to use powerline
+;; (and making my own small framework) and it just involved too much
+;; work to maintain a small feature.
 (use-package smart-mode-line
   :config
   (setq sml/no-confirm-load-theme t
 	sml/shorten-directory t
 	sml/theme 'respectful)
   (sml/setup))
+
+;; In spite of the name, I use this to make sure that when I scroll,
+;; there are still lines between the cursor and the top of the file.
+(use-package smooth-scrolling
+  :config
+  (setq smooth-scroll-margin 5
+	scroll-conservatively 101
+	scroll-preserve-screen-position t
+	auto-window-vscroll nil
+	scroll-margin 5)
+  (smooth-scrolling-mode 1))
+
+;; tern is a js navigation package which extends js-mode. TODO: Note that
+;; this is fairly hard to find, so it may be better to move this under
+;; a js-mode block.
+(use-package tern
+  :config
+  (use-package company-tern
+    :if (fboundp 'company-mode)
+    :config
+    (add-to-list 'company-backends 'company-tern)
+    (setq company-tern-property-marker ""))
+
+  (add-hook 'js-mode-hook (lambda () (tern-mode t))))
+
+;; Ensure we're using sane buffer naming
+(use-package uniquify
+  :ensure nil
+  :config
+  (setq uniquify-buffer-name-style 'forward))
+
+(use-package virtualenvwrapper)
+
+(use-package web-mode
+  :mode
+  "\\.jinja\\'"
+  "\\.html\\'"
+  :config
+  (setq web-mode-markup-indent-offset 2
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2))
+
+;; yasnippet adds some useful tools to make reusable code snippets.
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :config
+  (setq yas-verbosity 0)
+  (yas-global-mode 1))
+
+;;;; Tweaks
+;; TODO: This section is pretty much everything that didn't fit nicely
+;; into a use-package block. It would be nice to refactor this a bit.
 
 ;; We pick a super generic fallback so it should work everywhere.
 (defvar belak/frame-font "Monospace 12")
@@ -152,10 +405,8 @@
       tooltip-delay 0
       tooltip-short-delay 0)
 
-;;;; Tweaks
-
 ;; I find that when I want to use zap, I almost never want to include
-;; the next character
+;; the next character, so we replace zap-to-chat with zap-up-to-char.
 (autoload 'zap-up-to-char "misc")
 (global-set-key [remap zap-to-char] 'zap-up-to-char)
 
@@ -166,13 +417,6 @@
 ;; because that takes too many keystrokes.
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(require 'saveplace)
-(setq-default save-place t)
-
-;; Ensure we're using sane buffer naming
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
 (show-paren-mode 1)
 (setq show-paren-style 'expression
       show-paren-delay 0
@@ -182,13 +426,6 @@
       require-final-newline t
       load-prefer-newer t
       inhibit-splash-screen t)
-
-;; Grab important environment variables from the shell. The important
-;; ones are PATH and GOPATH.
-(use-package exec-path-from-shell
-  :config
-  (add-to-list 'exec-path-from-shell-variables "GOPATH")
-  (exec-path-from-shell-initialize))
 
 ;; As a former vim user, I like escape to actually quit
 ;; everywhere. This was taken from
@@ -210,56 +447,8 @@ abort the minibuffer."
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
-(require 'recentf)
-(recentf-mode 1)
-
-;; In spite of the name, I use this to make sure that when I scroll,
-;; there are still lines between the cursor and the top of the file.
-(use-package smooth-scrolling
-  :config
-  (setq smooth-scroll-margin 5
-	scroll-conservatively 101
-	scroll-preserve-screen-position t
-	auto-window-vscroll nil
-	scroll-margin 5)
-  (smooth-scrolling-mode 1))
-
-;;;; Navigation
-(use-package projectile
-  :diminish projectile-mode
-  :config
-  (projectile-global-mode))
-
-(use-package ido
-  :config
-  (use-package smex
-    :bind
-    ("M-x" . smex)
-    ("M-X" . smex-major-mode-commands))
-
-  (use-package ido-ubiquitous
-    :config
-    (ido-ubiquitous-mode 1))
-
-  (use-package ido-vertical-mode
-    :config
-    (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right
-	  ido-vertical-show-count t)
-    (ido-vertical-mode 1))
-
-  (use-package flx-ido
-    :config
-    (setq ido-enable-flex-matching t
-	  flx-ido-threshold 1000))
-
-  (setq resize-mini-windows t
-	ido-use-virtual-buffers t
-	ido-auto-merge-work-directories-length -1)
-
-  (ido-mode 1)
-  (ido-everywhere 1))
-
-;;;; Code
+;; Ensure all trailing whitespace is removed
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Highlight between matching parens
 (electric-pair-mode 1)
@@ -270,167 +459,17 @@ abort the minibuffer."
 ;; Delete text if we start typing
 (delete-selection-mode)
 
+;; This is a common hook for all modes that are based on the generally
+;; programming mode.
 (add-hook 'prog-mode-hook
 	  (lambda ()
 	    (linum-mode 1)
 	    (setq show-trailing-whitespace t)))
 
-;; fic-mode simply gives an annoying highlight to FIXME, TODO, XXX,
-;; and other similar keywords so they're easy to spot.
-(use-package fic-mode
-  :diminish fic-mode
-  :config
-  (add-to-list 'fic-highlighted-words "XXX")
-  (add-hook 'prog-mode-hook 'fic-mode))
-
-;; editorconfig is a simple way to share indentation settings between
-;; editors. Because I sometimes dabble in neovim and sublime, it's
-;; nice to not have to re-do these settings at a project level between
-;; editors.
-(use-package editorconfig
-  :config
-  (editorconfig-mode 1))
-
-;; company-mode is used as a completion system. In use-package blocks,
-;; you can use :if (fboundp 'company-mode) to only enable a block if
-;; company-mode is active.
-(use-package company
-  :diminish company-mode
-  :config
-  (setq company-idle-delay 0
-	company-echo-delay 0
-	company-minimum-prefix-length 1)
-  (global-company-mode))
-
-;; flycheck-mode is used for linters and catching compilation
-;; errors. In use-package blocks, you can use :if (fboundp
-;; 'flycheck-mode) to only enable a block if flycheck-mode is enabled.
-(use-package flycheck
-  :diminish flycheck-mode
-  :config
-  (defalias 'flycheck-show-error-at-point-soon 'flycheck-show-error-at-point)
-  (global-flycheck-mode))
-
-;; yasnippet adds some useful tools to make reusable code snippets.
-(use-package yasnippet
-  :diminish yas-minor-mode
-  :config
-  (setq yas-verbosity 0)
-  (yas-global-mode 1))
-
-;;; VCS tools
-(use-package magit
-  :bind ("M-g M-g" . magit-status)
-  :init
-  (use-package magit-filenotify
-    :if (linux-p)
-    :config
-    (add-hook 'magit-status-mode-hook 'magit-filenotify-mode))
-  :config
-  (setq magit-push-always-verify t
-	magit-completing-read-function 'magit-ido-completing-read))
-
-(use-package diff-hl
-  :config
-  (add-hook 'after-make-console-frame-hooks
-	    (lambda ()
-	      (message "Console")
-	      (global-diff-hl-mode 0)
-	      (diff-hl-margin-mode 1)))
-  (add-hook 'after-make-window-system-frame-hooks
-	    (lambda ()
-	      (message "Graphical")
-	      (global-diff-hl-mode 1)
-	      (diff-hl-margin-mode 0))))
-
 ;; Revert buffers automatically if they've changed on diskx
 (global-auto-revert-mode 1)
 (setq auto-revert-verbose nil)
-
 (diminish 'auto-revert-mode)
-
-;;; text-mode
-
-;; flyspell does what it says on the tin. It's a spell-checker similar to flycheck.
-(use-package flyspell
-  :diminish flyspell-mode
-  :config (add-hook 'text-mode-hook (lambda () (flyspell-mode 1))))
-
-;;; Python
-
-(use-package anaconda-mode
-  :diminish anaconda-mode
-  :config
-  (use-package company-anaconda
-    :if (fboundp 'company-mode)
-    :config (add-to-list 'company-backends 'company-anaconda))
-
-  (when (fboundp 'projectile-mode)
-    (defun belak/virtualenv-magic (&rest arg-list)
-      (if (and (projectile-project-p) (venv-is-valid (projectile-project-name)))
-	  (venv-workon (projectile-project-name))))
-    (advice-add 'switch-to-buffer :after #'belak/virtualenv-magic))
-
-  (add-hook 'python-mode-hook 'anaconda-mode))
-
-(use-package pip-requirements
-  :mode
-  "requirements.txt"
-  "requirements/\\.txt\\'")
-
-(use-package virtualenvwrapper)
-
-;;; Web
-
-(use-package rainbow-mode
-  :commands rainbow-mode)
-
-(use-package web-mode
-  :mode
-  "\\.jinja\\'"
-  "\\.html\\'"
-  :config
-  (setq web-mode-markup-indent-offset 2
-	web-mode-css-indent-offset 2
-	web-mode-code-indent-offset 2))
-
-(use-package less-css-mode
-  :mode "\\.less\\'")
-
-(use-package js2-mode
-  :mode "\\.js\\'"
-  :config
-  (setq js2-basic-offset 2)
-  (when (fboundp 'flycheck-mode)
-    (set-face-attributes 'js2-error nil
-			 :inherit 'flycheck-error-list-error
-			 :underline '(:color foreground-color :style wave))
-    (set-face-attributes 'js2-warning nil
-			 :inherit 'flycheck-error-list-warning
-			 :underline '(:color foreground-color :style wave))))
-
-(use-package tern
-  :config
-  (use-package company-tern
-    :if (fboundp 'company-mode)
-    :config
-    (add-to-list 'company-backends 'company-tern)
-    (setq company-tern-property-marker ""))
-
-  (add-hook 'js-mode-hook (lambda () (tern-mode t))))
-
-;;; Various languages
-
-(use-package go-mode
-  :mode "\\.go\\'"
-  :config
-  (use-package company-go
-    :if (fboundp 'company-mode)
-    :config
-    (add-to-list 'company-backends 'company-go))
-
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (setq gofmt-command "goimports"))
 
 ;;;; Cleanup
 
