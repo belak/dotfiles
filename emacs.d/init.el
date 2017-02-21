87c98d2a01fa576d95bf4825b1b778d2a8c65c3b
Kaleb Elwert <belak@coded.io>
2017-02-21 11:20:06 -0800

    Move some startup timing to a more accurate place

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,14 +8,13 @@
 
 ;; Define the start time so we can measure how long loading took
 ;; later.
+(defconst emacs-start-time (current-time))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-
-(defconst emacs-start-time (current-time))
 
 ;; Set the gc-cons-threshold as early as possible so we have some
 ;; extra memory to work with. This increases it to 20M.
