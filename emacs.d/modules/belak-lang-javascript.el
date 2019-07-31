bb8b432aae04c86611537193d305ba160acb0c63
Kaleb Elwert <belak@coded.io>
2019-07-31 14:25:37 -0700

    emacs: add typescript

--- emacs.d/modules/belak-lang-javascript.el
+++ emacs.d/modules/belak-lang-javascript.el
@@ -7,6 +7,9 @@
 (use-package js2-mode
   :mode ("\\.js\\'" . js2-jsx-mode))
 
+(use-package typescript-mode
+  :mode ("\\.tsx?\\'" . typescript-mode))
+
 (provide 'belak-lang-javascript)
 
 ;;; belak-lang-javascript.el ends here
