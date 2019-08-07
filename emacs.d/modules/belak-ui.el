9435c3be77ac0fecef880da189f86ebf898354f8
Kaleb Elwert <belak@coded.io>
2019-08-07 03:56:24 -0700

    emacs: rewrite to use org

--- emacs.d/modules/belak-ui.el
+++ emacs.d/modules/belak-ui.el
@@ -1,63 +1,11 @@
-;;; belak-ui --- UI related settings and packages
-
-;;; Commentary:
-
-;;; Code:
-
-;; Remove most gui features because I rarely use any of them.
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
-
-(blink-cursor-mode -1)
-
-(setq echo-keystrokes 0.1)
-
 ;; Various appearance settings
 
 (setq column-number-mode t
-      line-number-mode t
-      tooltip-delay 0
-      tooltip-short-delay 0)
-
-;; Ensure the help window is selected when one is open. This makes it
-;; much easier to quit them when we're done.
-
-(setq help-window-select t)
+      line-number-mode t)
 
 ;; Highlight between matching parens
 
 (electric-pair-mode 1)
-
-;; Highlight the current column
-
-(global-hl-line-mode)
-
-;; Packages
-
-(use-package base16-theme
-  :disabled t
-  :config (load-theme 'base16-default-dark t))
-
-(use-package grayscale-theme
-  :disabled t
-  :config (load-theme 'grayscale t))
-
-(use-package monokai-pro-theme
-  :config (load-theme 'monokai-pro t))
-
-;; I originally used fic-mode, but it appears that hl-todo is a little
-;; better and is updated more frequently.
-
-(use-package hl-todo
-  :config
-  (global-hl-todo-mode))
-
-(use-package hlinum
-  :config
-  (hlinum-activate))
 
 (use-package paren
   :straight nil
@@ -65,25 +13,6 @@
   (show-paren-mode 1)
   (setq show-paren-style 'parenthesis
         show-paren-delay 0))
-
-(use-package spaceline-config
-  :straight spaceline
-  :config
-  (setq powerline-default-separator 'bar
-        spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-  (spaceline-spacemacs-theme))
-
-(use-package which-key
-  :defer 1
-  :diminish which-key-mode
-  :config
-  (setq which-key-sort-order #'which-key-prefix-then-key-order
-        which-key-sort-uppercase-first nil
-        which-key-add-column-padding 1
-        which-key-max-display-columns nil
-        which-key-min-display-lines 6
-        which-key-side-window-slot -10)
-  (which-key-mode 1))
 
 (use-package whitespace
   :straight nil
@@ -95,11 +24,3 @@
                                       (tab-mark 9 [9655 9] [92 9])))
   (global-whitespace-mode t)
   (setq whitespace-global-modes '(text-mode prog-mode org-mode)))
-
-;; Additional modes to diminish
-(diminish 'auto-fill-function)
-(use-package undo-tree
-  :diminish undo-tree-mode)
-
-(provide 'belak-ui)
-;;; belak-ui.el ends here
