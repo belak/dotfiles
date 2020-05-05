db941ceb120f453864986f506bdea993b2cc23cd
Kaleb Elwert <belak@coded.io>
2020-05-04 20:04:24 -0700

    doom: various tweaks

---
+++ doom.d/+ui.el
@@ -0,0 +1,13 @@
+;;; $DOOMDIR/+ui.el -*- lexical-binding: t; -*-
+
+(setq doom-font (font-spec :family "Source Code Pro" :size 14)
+      ;;doom-theme 'monokai-pro
+      doom-theme 'modus-vivendi
+      doom-modeline-icon t
+      doom-modeline-buffer-file-name-style  'truncate-upto-root)
+
+(setq modus-vivendi-theme-visible-fringe t)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
--- doom.d/config.el
+++ doom.d/config.el
@@ -5,21 +5,9 @@
 (setq user-full-name "Kaleb Elwert"
       user-mail-address "belak@coded.io")
 
-;; UI
-(setq doom-font (font-spec :family "Source Code Pro" :size 14)
-      ;;doom-theme 'monokai-pro
-      doom-theme 'modus-vivendi
-      doom-modeline-icon nil)
-
-(setq modus-vivendi-theme-visible-fringe t)
-
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
 
 (setq uniquify-buffer-name-style 'forward
       uniquify-strip-common-suffix nil)
@@ -36,7 +24,7 @@
 (global-set-key "\M-Z" 'reverse-zap-up-to-char)
 
 (after! ido
-  (setq ido-use-virtual-buffers t)
+  (setq ido-use-virtual-buffers t))
 
-  (map! "M-x" #'smex
-        "M-X" #'smex-major-mode-commands))
+(load! "+bindings")
+(load! "+ui")
