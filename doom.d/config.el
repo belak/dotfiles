f01e1cd69c802db0e13d72d95f961593ce99916e
Kaleb Elwert <belak@coded.io>
2020-05-27 12:27:33 -0700

    doom: change default font and change some bindings

--- doom.d/config.el
+++ doom.d/config.el
@@ -5,6 +5,12 @@
 (setq user-full-name "Kaleb Elwert"
       user-mail-address "belak@coded.io")
 
+;; UI
+(setq doom-font (font-spec :family "Source Code Pro" :size 12)
+      ;;doom-theme 'monokai-pro
+      doom-theme 'modus-vivendi
+      doom-modeline-icon nil)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
@@ -28,3 +34,14 @@
 
 (load! "+bindings")
 (load! "+ui")
+
+(map!
+ ;; Allow C-a and C-e to work in normal mode as well.
+ ;; TODO: make sure they also enter insert mode
+ :n "C-a" #'doom/backward-to-bol-or-indent
+ :n "C-e" #'doom/forward-to-last-non-comment-or-eol
+
+ ;; Make home and end do the same thing as C-a/C-e rather than going to the
+ ;; beginning/end of a buffer.
+ :g "<home>" #'doom/backward-to-bol-or-indent
+ :g "<end>"  #'doom/forward-to-last-non-comment-or-eol)
