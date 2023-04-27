03215ba2f8a576eb4d8b964e046a8de390ca7c29
Kaleb Elwert <belak@coded.io>
2023-04-27 10:43:29 -0700

    finicky: add package tracking to personal browser

--- finicky.js
+++ finicky.js
@@ -6,16 +6,26 @@ module.exports = {
   // All my work computers have had long generated names starting with COMP_, so
   // I use that to decide if I'm using Google Chrome or Firefox by default.
   defaultBrowser: localizedName.startsWith("COMP_") ? "Google Chrome" : "Firefox",
+  options: {
+    logRequests: true,
+  },
   handlers: [
     {
       // Open YouTube URLs in Firefox so I can have my work profile in Google,
-      // and
+      // and personal stuff in firefox.
 
-      match: finicky.matchDomains([
+      match: finicky.matchHostnames([
         'www.youtube.com',
         'm.youtube.com',
         'youtube.com',
         'youtu.be'
+      ]),
+      browser: "Firefox"
+    },
+    {
+      // Open package tracking links in Firefox.
+      match: finicky.matchHostnames([
+        'parcel.app',
       ]),
       browser: "Firefox"
     }
