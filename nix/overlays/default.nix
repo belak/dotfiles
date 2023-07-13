141c9994d23cf7f148d79411bc8e72c3d841534b
Kaleb Elwert <belak@coded.io>
2023-07-13 00:37:40 -0700

    nix: add wezterm-bin and other tweaks

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,7 +1,13 @@
 {
   packages = (final: prev: {
-    my.wezterm = final.darwin.apple_sdk_11_0.callPackage ./packages/wezterm.nix {
-      inherit (final.darwin.apple_sdk_11_0.frameworks) Cocoa CoreGraphics Foundation UserNotifications System;
+    # Overrides
+    my = {
+      wezterm = final.darwin.apple_sdk_11_0.callPackage ./packages/wezterm.nix {
+        inherit (final.darwin.apple_sdk_11_0.frameworks) Cocoa CoreGraphics Foundation UserNotifications System;
+      };
     };
+
+    # Additional packages
+    wezterm-bin = final.callPackage ./packages/wezterm-bin.nix { };
   });
 }
---
+++ nix/overlays/packages/wezterm-bin.nix
@@ -0,0 +1,72 @@
+{ stdenv
+, lib
+, fetchurl
+, perl
+, pkg-config
+, python3
+, fontconfig
+, openssl
+, libX11
+, libxcb
+, libxkbcommon
+, wayland
+, xcbutil
+, xcbutilimage
+, xcbutilkeysyms
+, xcbutilwm
+, zlib
+, libiconv
+}:
+
+let
+  rpath = lib.makeLibraryPath ([
+    fontconfig
+    zlib
+  ] ++ lib.optionals stdenv.isLinux [
+    libX11
+    libxcb
+    libxkbcommon
+    openssl
+    wayland
+    xcbutil
+    xcbutilimage
+    xcbutilkeysyms
+    xcbutilwm
+  ] ++ lib.optionals stdenv.isDarwin [
+    libiconv
+  ]);
+in
+stdenv.mkDerivation rec {
+  pname = "wezterm-bin";
+  version = "20230712-072601-f4abf8fd";
+
+  src = fetchurl {
+    url = "https://github.com/wez/wezterm/releases/download/${version}/wezterm-${version}.Ubuntu22.04.tar.xz";
+    hash = "sha256-OwO3WiajDy3igYDWbFVW5YePbzMg150S+3jqSGQAtsM=";
+  };
+
+  #nativeBuildInputs = [ pkg-config python3 perl ];
+
+  # prevent further changes to the RPATH
+  dontPatchELF = true;
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r ./usr/* $out/
+  '';
+
+  postFixup = ''
+    for artifact in wezterm wezterm-gui wezterm-mux-server strip-ansi-escapes; do
+     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/$artifact" || true
+     patchelf --set-rpath "${rpath}" $out/bin/$artifact
+    done
+  '';
+
+  meta = with lib; {
+    description = "GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust (Pre-compiled version)";
+    homepage = "https://github.com/wez/wezterm";
+    license = licenses.mit;
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
