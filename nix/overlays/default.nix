eb20a5ffbcc27fc7d3db8927e1825524754b3e52
Kaleb Elwert <belak@coded.io>
2023-07-13 00:40:23 -0700

    nix: remove wezterm from overlay

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,11 +1,7 @@
 {
   packages = (final: prev: {
     # Overrides
-    my = {
-      wezterm = final.darwin.apple_sdk_11_0.callPackage ./packages/wezterm.nix {
-        inherit (final.darwin.apple_sdk_11_0.frameworks) Cocoa CoreGraphics Foundation UserNotifications System;
-      };
-    };
+    my = { };
 
     # Additional packages
     wezterm-bin = final.callPackage ./packages/wezterm-bin.nix { };
