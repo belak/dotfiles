687503742dd7a48c76fb28f7775e4f1015ea9fb7
Kaleb Elwert <kaleb@coded.io>
2023-07-17 03:51:01 -0700

    nix: set hostname on auron

--- nix/hosts/nixos/auron/default.nix
+++ nix/hosts/nixos/auron/default.nix
@@ -24,6 +24,11 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+  networking = {
+    hostName = "auron";
+    domain = "elwert.dev";
+  };
+
   # Set your time zone.
   time.timeZone = "Etc/UTC";
 
