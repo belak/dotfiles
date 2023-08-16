0893605e5a7e2be1a88e7ddf195590a50a026156
Kaleb Elwert <belak@coded.io>
2023-08-16 15:52:53 -0700

    finicky: add some github urls

--- finicky.js
+++ finicky.js
@@ -2,12 +2,12 @@
 
 let { localizedName, name } = finicky.getSystemInfo();
 
-const isWorkComputer = localizedName.startsWith("COMP_");
+const isWorkComputer = localizedName.startsWith('COMP_');
 
 module.exports = {
   // All my work computers have had long generated names starting with COMP_, so
   // I use that to decide if I'm using Google Chrome or Firefox by default.
-  defaultBrowser: isWorkComputer ? "Google Chrome" : "Firefox",
+  defaultBrowser: isWorkComputer ? 'Google Chrome' : 'Firefox',
   options: {
     logRequests: true,
   },
@@ -15,7 +15,6 @@ module.exports = {
     {
       // Open YouTube and Steam URLs in Firefox so I can have my work profile in Google,
       // and personal stuff in firefox.
-
       match: finicky.matchHostnames([
         'store.steampowered.com',
         'www.youtube.com',
@@ -23,14 +22,22 @@ module.exports = {
         'youtube.com',
         'youtu.be'
       ]),
-      browser: "Firefox"
+      browser: 'Firefox'
     },
     {
       // Open package tracking links in Firefox.
       match: finicky.matchHostnames([
         'parcel.app',
       ]),
-      browser: "Firefox"
+      browser: 'Firefox'
+    },
+    {
+      // Ensure personal projects open in Firefox.
+      match: [
+        'github.com/belak/*',
+        'github.com/seabird-chat/*'
+      ],
+      browser: 'Firefox'
     }
   ]
 }
