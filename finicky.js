c16501c46b16f27f10d3be22b6ba4a460416fd4e
Kaleb Elwert <belak@coded.io>
2023-11-15 08:36:41 -0800

    finicky: include some tweaks to directly open URLs in apps

--- finicky.js
+++ finicky.js
@@ -13,6 +13,35 @@ module.exports = {
   },
   handlers: [
     {
+      // This works in concert with the zoom rewrite section below to open Zoom
+      // links in the Zoom app.
+      match: /zoom\.us\/join/,
+      browser: "us.zoom.xos"
+    },
+    {
+      // Open any link from Discord in Firefox
+      match: ({opener}) =>
+          ["com.hnc.Discord"].includes(opener.bundleId),
+      browser: "Firefox"
+    },
+    {
+      // Open any link from Slack in Google Chrome
+      match: ({opener}) =>
+          ["com.tinyspeck.slackmacgap"].includes(opener.bundleId),
+      browser: "Google Chrome"
+    },
+    {
+      // Force Spotify links to actually open in the Spotify app
+      match: finicky.matchDomains("open.spotify.com"),
+      browser: "Spotify"
+    },
+    {
+      // Force Discord links to open in the Discord app
+      match: "https://discord.com/*",
+      url: { protocol: "discord" },
+      browser: "Discord",
+    },
+    {
       // Open YouTube and Steam URLs in Firefox so I can have my work profile in Google,
       // and personal stuff in firefox.
       match: finicky.matchHostnames([
@@ -38,6 +67,27 @@ module.exports = {
         'github.com/seabird-chat/*'
       ],
       browser: 'Firefox'
+    }
+  ],
+
+  rewrite: [
+    {
+      // Rewrite Zoom "join" links to be protocol links so they work properly
+      // with the Zoom app.
+      match: ({ url }) => url.host.includes("zoom.us") && url.pathname.includes("/j/"),
+      url: ({ url }) => {
+        try {
+          var pass = '&pwd=' + url.search.match(/pwd=(\w*)/)[1];
+        } catch {
+          var pass = ""
+        }
+        var conf = 'confno=' + url.pathname.match(/\/j\/(\d+)/)[1];
+        return {
+          search: conf + pass,
+          pathname: '/join',
+          protocol: "zoommtg"
+        }
+      }
     }
   ]
 }
