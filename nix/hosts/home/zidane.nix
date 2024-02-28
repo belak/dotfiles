3d9a2701c8cbf787811efbeb698e5969af875f02
Kaleb Elwert <belak@coded.io>
2024-02-28 01:02:15 -0800

    nix: add home.stateVersion to zidane

--- nix/hosts/home/zidane.nix
+++ nix/hosts/home/zidane.nix
@@ -2,4 +2,13 @@
   belak = {
     dotfiles.enable = true;
   };
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
 }
