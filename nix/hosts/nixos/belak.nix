31ae4242bab0ec9044f94b66bfb25cd5c23bc44e
Kaleb Elwert <belak@coded.io>
2024-04-21 14:55:51 -0700

    nix: update belak group handling

--- nix/hosts/nixos/belak.nix
+++ nix/hosts/nixos/belak.nix
@@ -1,14 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  extraGroups = ifTheyExist [ "networkmanager" ];
+in
 {
   users.users.belak = {
+    home = "/home/belak";
     isNormalUser = true;
     initialPassword = "hunter2";
     description = "Kaleb Elwert";
     extraGroups = [
-      "networkmanager"
       "wheel"
       "dialout"
-    ];
+    ] ++ extraGroups;
     shell = pkgs.zsh;
 
     # subuid and subgid allows podman to work as expected.
