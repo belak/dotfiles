b754e44627cfe7368202ad16a563d3c560076153
Kaleb Elwert <belak@coded.io>
2024-06-01 09:45:19 -0700

    nix: disable a few broken packages and overlays

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -7,15 +7,15 @@
 {
   additions = final: _prev: { my = import ./pkgs { pkgs = final; }; };
 
-  deploy-rs-overlay = deploy-rs.overlays.default;
+  #deploy-rs-overlay = deploy-rs.overlays.default;
 
   # This is a modified version of what's in the deploy-rs readme. For some
   # reason overriding deploy-rs entirely seems to clobber the needed `lib` attr.
-  deploy-rs-pkg-override = final: prev: {
-    deploy-rs = prev.deploy-rs // {
-      deploy-rs = nixpkgs-unstable.legacyPackages.${final.system}.deploy-rs;
-    };
-  };
+  #deploy-rs-pkg-override = final: prev: {
+  #  deploy-rs = prev.deploy-rs // {
+  #    deploy-rs = nixpkgs-unstable.legacyPackages.${final.system}.deploy-rs;
+  #  };
+  #};
 
   unstable = final: _prev: { unstable = self.lib.mkPkgs final.system nixpkgs-unstable [ ]; };
 }
