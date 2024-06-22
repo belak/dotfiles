2708e848b7adea648888a6115bcf232fa7bc9f7c
Kaleb Elwert <belak@coded.io>
2024-06-22 15:46:46 -0700

    nix: use cached deploy-rs instance

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -10,12 +10,16 @@
   deploy-rs-overlay = deploy-rs.overlays.default;
 
   # This is a modified version of what's in the deploy-rs readme. For some
-  # reason overriding deploy-rs entirely seems to clobber the needed `lib` attr.
+  # reason overriding deploy-rs entirely seems to clobber the needed `lib` attr,
+  # and oddly enough the `deploy-rs` binary is accessed through
+  # deploy-rs.reploy-rs, so we need to provide that as well.
+  #
   # Note that this depends on our "unstable" overlay because it's the easiest
   # way to make sure we get a package from nixpkgs and not from the deploy-rs
-  # source.
+  # source. Another option would be creating a new nixpkgs instance.
   deploy-rs-pkg-override = final: prev: {
     deploy-rs = final.unstable.deploy-rs // {
+      inherit (final.unstable) deploy-rs;
       inherit (prev.deploy-rs) lib;
     };
   };
