bfe7551f57472c8dcc1313e43de15b5200217cf8
Kaleb Elwert <belak@coded.io>
2024-09-06 21:10:56 -0700

    nix: move cosmic cache to cosmic module

--- nix/nixos/modules/cosmic.nix
+++ nix/nixos/modules/cosmic.nix
@@ -12,6 +12,12 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    # If cosmic is enabled, we want to make sure we enable the provided cache.
+    nix.settings = {
+      substituters = [ "https://cosmic.cachix.org/" ];
+      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+    };
+
     services.desktopManager.cosmic.enable = true;
     services.displayManager.cosmic-greeter.enable = true;
   };
