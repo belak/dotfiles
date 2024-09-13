d873da460ab33647501735f99d218b5172edc9ba
Kaleb Elwert <belak@coded.io>
2024-09-13 15:15:48 -0700

    emacs: disable drag-stuff

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -12,3 +12,6 @@
 ;; buffer-name-relative is needed as a workaround because the uniquify settings
 ;; don't work with persp-mode, which doom currently uses for workspaces.
 (package! buffer-name-relative)
+
+;; Disable drag-stuff because the bindings conflict with M-left and M-right.
+(package! drag-stuff :disable t)
