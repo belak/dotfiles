2514468980664f5882d013c6c92631e1f743e293
Kaleb Elwert <belak@coded.io>
2024-09-25 22:02:53 -0700

    wezterm: switch to modus vivendi colors

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -65,42 +65,7 @@ local config = {
 
   -- None of the color schemes quite match the default Linux colors, so we
   -- define our own.
-  color_scheme = "Linux",
-  color_schemes = {
-    ["Linux"] = {
-      background = "#000000",
-      foreground = "#ffffff",
-
-      -- NOTE: these aren't quite right - I should probably tweak them at some
-      -- point.
-      cursor_bg = "#ffffff",
-      cursor_border = "#ffffff",
-      cursor_fg = "#000000",
-      selection_bg = "#4d52f8",
-      selection_fg = "#000000",
-
-      ansi = {
-        "#000000",
-        "#aa0000",
-        "#00aa00",
-        "#aa5500",
-        "#0000aa",
-        "#aa00aa",
-        "#00aaaa",
-        "#aaaaaa",
-      },
-      brights = {
-        "#555555",
-        "#ff5555",
-        "#55ff55",
-        "#ffff55",
-        "#5555ff",
-        "#ff55ff",
-        "#55ffff",
-        "#ffffff"
-      },
-    }
-  }
+  color_scheme = "Modus Vivendi",
 }
 
 if is_linux then
