c90d409eebd0822be9e4d5c4dc5ff74f95ce6a99
Kaleb Elwert <belak@coded.io>
2024-11-30 16:56:12 -0800

    nix: drop sound.enable to fix zorn and zagreus

--- nix/nixos/hosts/zagreus/default.nix
+++ nix/nixos/hosts/zagreus/default.nix
@@ -53,7 +53,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
