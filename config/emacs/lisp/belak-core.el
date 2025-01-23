19828c02bea4d21e0fa5dc6a2146c17450e78b6c
Kaleb Elwert <belak@coded.io>
2025-01-22 23:03:30 -0800

    emacs: fix packages broken by updating

--- config/emacs/lisp/belak-core.el
+++ config/emacs/lisp/belak-core.el
@@ -119,18 +119,6 @@
 
 
 ;;
-;;; Dependency Workarounds
-
-;; eldoc and jsonrpc are both built-in dependencies needed by eglot, but they're not new enough,
-;; so we have to install a different version using elpaca.
-(use-package! eldoc)
-(use-package! jsonrpc)
-
-;; seq is needed by flycheck and transient (which is needed by magit, rg, and forge)
-(use-package! seq)
-
-
-;;
 ;;; Optimizations
 
 ;; `gcmh-mode' is a long standing hack which tweaks the garbage collection to be
