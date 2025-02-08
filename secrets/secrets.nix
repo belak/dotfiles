deca78f52dc50fa198aca8e4380f7007e32b4a9d
Kaleb Elwert <belak@coded.io>
2025-02-07 21:35:27 -0800

    nix: add quina to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,14 @@
 let
   user-belak-work = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUSx9TTTHUq4GOkeBU4Ga03QombEBiZLqqa8KIqnnUy";
   user-belak-melinoe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMzuXboQDv2VCig0+A780O0+sKs1euw+3OafnRA6z14P";
+  user-belak-quina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdD1/ti5UMpCjCJbwTGplZZmVNwCT+c6rYqdKa9cN3t";
   user-belak-zagreus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNHMEfjGg5ek6OtbFytZ/zCSZosT8aHqHRfnufb3gIi";
   user-belak-zorn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvdWkVKcV087KDa9e2fdaubwW8SztSo+k+lYaeEKILC";
 
   users = [
     user-belak-work
     user-belak-melinoe
+    user-belak-quina
     user-belak-zagreus
     user-belak-zorn
   ];
@@ -16,6 +18,7 @@ let
   system-stiltzkin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCdp7P84mEIR2+TwvqjXlqye92bCOSD1uaf+kuBl2ex";
   system-moguo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILR5itUnf60cPaUMbwe2O+pPr99zXyCy6S1oGg2bOub3";
   system-monty = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+wnueT1cqvNDi5INmEsFSHHW5/Chhko8LGEz9zQOPx";
+  system-quina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyYqXHUJt+EZhWhWH2WlErat+CQQ/iVfsZNkaJLzEBu";
   system-eiko = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFpH5p7ODkUq0kLqda1/fghcCo+MxvCZLdKOfhZCtK+";
   system-vivi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDx5Y7VvA9CUdrsiVpNbRufBdJdvJZEfRQXIGnPgqynH";
   system-zagreus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID4UJL+NcFXmr678LkhV92sqIoM4gpKic0lc8DGH0Mib";
@@ -28,6 +31,7 @@ let
     system-stiltzkin
     system-moguo
     system-monty
+    system-quina
     system-eiko
     system-vivi
     system-zagreus
