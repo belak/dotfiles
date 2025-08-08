2531e86c24fef1b9c0b55a429cf8e39130a8612a
Kaleb Elwert <belak@coded.io>
2025-08-07 23:56:34 -0700

    nix: open port 80 on nginx hosts

--- nix/nixos/modules/services/nginx.nix
+++ nix/nixos/modules/services/nginx.nix
@@ -22,6 +22,10 @@ in
 
     users.groups.nginx = { };
 
+    networking.firewall.allowedTCPPorts = [
+      80
+    ];
+
     # Hacks - disable ProtectHome so we can access sockets outside our home.
     # There should be a better way to do this, but this works for now.
     systemd.services.nginx.serviceConfig.ProtectHome = false;
