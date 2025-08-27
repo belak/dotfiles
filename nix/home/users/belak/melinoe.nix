3cac48831596aab59604a44ea4eed3bdd0b23f09
Kaleb Elwert <belak@coded.io>
2025-08-27 01:38:27 -0700

    nix: update vscode settings

--- nix/home/users/belak/melinoe.nix
+++ nix/home/users/belak/melinoe.nix
@@ -32,107 +32,109 @@
 
     mutableExtensionsDir = false;
 
-    extensions =
-      with pkgs.vscode-extensions;
-      [
-        editorconfig.editorconfig
-        golang.go
-        jnoortheen.nix-ide
-        stkb.rewrap
-        rust-lang.rust-analyzer
-      ]
-      ++ (with pkgs.community-vscode-extensions.vscode-marketplace; [
-        a-h.templ
-        monokai.theme-monokai-pro-vscode
-      ]);
+    profiles.default = {
+      extensions =
+        with pkgs.vscode-extensions;
+        [
+          editorconfig.editorconfig
+          golang.go
+          jnoortheen.nix-ide
+          stkb.rewrap
+          rust-lang.rust-analyzer
+        ]
+        ++ (with pkgs.community-vscode-extensions.vscode-marketplace; [
+          a-h.templ
+          monokai.theme-monokai-pro-vscode
+        ]);
 
-    userSettings = {
-      "editor.acceptSuggestionOnCommitCharacter" = false;
-      "editor.cursorBlinking" = "solid";
-      "editor.find.seedSearchStringFromSelection" = false;
-      "editor.formatOnSave" = true;
-      "editor.minimap.enabled" = false;
-      "editor.renderWhitespace" = "selection";
-      "editor.scrollBeyondLastLine" = false;
-      "explorer.fileNesting.expand" = false;
-      "extensions.ignoreRecommendations" = true;
-      "files.hotExit" = "off";
-      "workbench.colorTheme" = "Monokai Pro";
-      "workbench.iconTheme" = "Monokai Pro Icons";
-      "workbench.settings.editor" = "json";
-      "workbench.settings.useSplitJSON" = true;
-      "workbench.startupEditor" = "none";
+      userSettings = {
+        "editor.acceptSuggestionOnCommitCharacter" = false;
+        "editor.cursorBlinking" = "solid";
+        "editor.find.seedSearchStringFromSelection" = false;
+        "editor.formatOnSave" = true;
+        "editor.minimap.enabled" = false;
+        "editor.renderWhitespace" = "selection";
+        "editor.scrollBeyondLastLine" = false;
+        "explorer.fileNesting.expand" = false;
+        "extensions.ignoreRecommendations" = true;
+        "files.hotExit" = "off";
+        "workbench.colorTheme" = "Monokai Pro";
+        "workbench.iconTheme" = "Monokai Pro Icons";
+        "workbench.settings.editor" = "json";
+        "workbench.settings.useSplitJSON" = true;
+        "workbench.startupEditor" = "none";
 
-      "emmet.includeLanguages" = {
-        "templ" = "html";
+        "emmet.includeLanguages" = {
+          "templ" = "html";
+        };
+
+        "[nix]" = {
+          "editor.formatOnSave" = false;
+        };
+
+        "[templ]" = {
+          "editor.defaultFormatter" = "a-h.templ";
+        };
       };
 
-      "[nix]" = {
-        "editor.formatOnSave" = false;
-      };
-
-      "[templ]" = {
-        "editor.defaultFormatter" = "a-h.templ";
-      };
+      keybindings = [
+        {
+          "key" = "cmd+1";
+          "command" = "workbench.action.openEditorAtIndex1";
+        }
+        {
+          "key" = "cmd+2";
+          "command" = "workbench.action.openEditorAtIndex2";
+        }
+        {
+          "key" = "cmd+3";
+          "command" = "workbench.action.openEditorAtIndex3";
+        }
+        {
+          "key" = "cmd+4";
+          "command" = "workbench.action.openEditorAtIndex4";
+        }
+        {
+          "key" = "cmd+5";
+          "command" = "workbench.action.openEditorAtIndex5";
+        }
+        {
+          "key" = "cmd+6";
+          "command" = "workbench.action.openEditorAtIndex6";
+        }
+        {
+          "key" = "cmd+7";
+          "command" = "workbench.action.openEditorAtIndex7";
+        }
+        {
+          "key" = "cmd+8";
+          "command" = "workbench.action.openEditorAtIndex8";
+        }
+        {
+          "key" = "cmd+9";
+          "command" = "workbench.action.lastEditorInGroup";
+        }
+        {
+          "key" = "shift+cmd+1";
+          "command" = "workbench.action.focusFirstEditorGroup";
+        }
+        {
+          "key" = "shift+cmd+2";
+          "command" = "workbench.action.focusSecondEditorGroup";
+        }
+        {
+          "key" = "shift+cmd+3";
+          "command" = "workbench.action.focusThirdEditorGroup";
+        }
+        {
+          "key" = "ctrl+tab";
+          "command" = "workbench.action.nextEditorInGroup";
+        }
+        {
+          "key" = "ctrl+shift+tab";
+          "command" = "workbench.action.previousEditorInGroup";
+        }
+      ];
     };
-
-    keybindings = [
-      {
-        "key" = "cmd+1";
-        "command" = "workbench.action.openEditorAtIndex1";
-      }
-      {
-        "key" = "cmd+2";
-        "command" = "workbench.action.openEditorAtIndex2";
-      }
-      {
-        "key" = "cmd+3";
-        "command" = "workbench.action.openEditorAtIndex3";
-      }
-      {
-        "key" = "cmd+4";
-        "command" = "workbench.action.openEditorAtIndex4";
-      }
-      {
-        "key" = "cmd+5";
-        "command" = "workbench.action.openEditorAtIndex5";
-      }
-      {
-        "key" = "cmd+6";
-        "command" = "workbench.action.openEditorAtIndex6";
-      }
-      {
-        "key" = "cmd+7";
-        "command" = "workbench.action.openEditorAtIndex7";
-      }
-      {
-        "key" = "cmd+8";
-        "command" = "workbench.action.openEditorAtIndex8";
-      }
-      {
-        "key" = "cmd+9";
-        "command" = "workbench.action.lastEditorInGroup";
-      }
-      {
-        "key" = "shift+cmd+1";
-        "command" = "workbench.action.focusFirstEditorGroup";
-      }
-      {
-        "key" = "shift+cmd+2";
-        "command" = "workbench.action.focusSecondEditorGroup";
-      }
-      {
-        "key" = "shift+cmd+3";
-        "command" = "workbench.action.focusThirdEditorGroup";
-      }
-      {
-        "key" = "ctrl+tab";
-        "command" = "workbench.action.nextEditorInGroup";
-      }
-      {
-        "key" = "ctrl+shift+tab";
-        "command" = "workbench.action.previousEditorInGroup";
-      }
-    ];
   };
 }
