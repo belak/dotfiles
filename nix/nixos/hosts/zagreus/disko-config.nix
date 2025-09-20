c1a498677630e2c569f41b081770b7a7bb55dce4
Kaleb Elwert <belak@coded.io>
2025-09-19 21:14:19 -0700

    nix: tweak zagreus disko config

--- nix/nixos/hosts/zagreus/disko-config.nix
+++ nix/nixos/hosts/zagreus/disko-config.nix
@@ -1,19 +1,21 @@
 {
   disko.devices = {
     disk = {
-      main = {
+      zagreus = {
         device = "/dev/nvme0n1";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
+              name = "boot";
               type = "EF00";
-              size = "512M";
+              size = "1024M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
