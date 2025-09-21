4db230769a9cace14cdf0e85a258e4b5b4bd3a60
Kaleb Elwert <belak@coded.io>
2025-09-20 20:01:59 -0700

    nix: fix gnome extensions

--- nix/home/modules/gnome.nix
+++ nix/home/modules/gnome.nix
@@ -17,69 +17,73 @@ in
       dconf-editor
       gnome-firmware
       gnome-tweaks
-      gnomeExtensions.unite
     ];
 
-    dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-        enable-hot-corners = false;
-        font-antialiasing = "rgba";
-        gtk-theme = "Adwaita-dark";
-      };
-      "org/gnome/mutter" = {
-        #dynamic-workspaces = true;
-        edge-tiling = true;
-      };
-      "org/gnome/shell" = {
-        enabled-extensions = [
-          "unite@hardpixel.eu"
-          "user-theme@gnome-shell-extensions.gcampax.github.com"
-        ];
-        favorite-apps = [
-          "firefox.desktop"
-          "org.wezfurlong.wezterm.desktop"
-          "discord.desktop"
-        ];
-      };
-      "org/gnome/shell/extensions/unite" = {
-        hide-activities-button = "never";
-        hide-window-titlebars = "both";
-        reduce-panel-spacing = false;
-        show-window-buttons = "both";
-        show-window-title = "both";
-        use-activities-text = false;
-      };
-      "org/gnome/tweaks" = {
-        show-extensions-notice = false;
-      };
+    dconf = {
+      enable = true;
 
-      # Keybinds
-      "org/gnome/desktop/wm/keybindings" = lib.listToAttrs (
-        lib.concatLists (
-          map (n: [
-            {
-              name = "move-to-workspace-${toString n}";
-              value = [ "<Super><Shift>${toString n}" ];
-            }
-            {
-              name = "switch-to-workspace-${toString n}";
-              value = [ "<Super>${toString n}" ];
-            }
-          ]) (lib.range 1 9)
-        )
-      );
+      settings = {
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+          font-antialiasing = "rgba";
+          gtk-theme = "Adwaita-dark";
+        };
+        "org/gnome/mutter" = {
+          #dynamic-workspaces = true;
+          edge-tiling = true;
+        };
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = with pkgs.gnomeExtensions; [
+            unite.extensionUuid
+            "user-theme@gnome-shell-extensions.gcampax.github.com"
+          ];
+          favorite-apps = [
+            "firefox.desktop"
+            "org.wezfurlong.wezterm.desktop"
+            "discord.desktop"
+          ];
+        };
+        "org/gnome/shell/extensions/unite" = {
+          hide-activities-button = "never";
+          hide-window-titlebars = "both";
+          reduce-panel-spacing = false;
+          show-window-buttons = "both";
+          show-window-title = "both";
+          use-activities-text = false;
+        };
+        "org/gnome/tweaks" = {
+          show-extensions-notice = false;
+        };
 
-      "org/gnome/shell/keybindings" = lib.listToAttrs (
-        lib.concatLists (
-          map (n: [
-            {
-              name = "switch-to-application-${toString n}";
-              value = [ ];
-            }
-          ]) (lib.range 1 9)
-        )
-      );
+        # Keybinds
+        "org/gnome/desktop/wm/keybindings" = lib.listToAttrs (
+          lib.concatLists (
+            map (n: [
+              {
+                name = "move-to-workspace-${toString n}";
+                value = [ "<Super><Shift>${toString n}" ];
+              }
+              {
+                name = "switch-to-workspace-${toString n}";
+                value = [ "<Super>${toString n}" ];
+              }
+            ]) (lib.range 1 9)
+          )
+        );
+
+        "org/gnome/shell/keybindings" = lib.listToAttrs (
+          lib.concatLists (
+            map (n: [
+              {
+                name = "switch-to-application-${toString n}";
+                value = [ ];
+              }
+            ]) (lib.range 1 9)
+          )
+        );
+      };
     };
   };
 }
--- nix/nixos/modules/gnome.nix
+++ nix/nixos/modules/gnome.nix
@@ -16,23 +16,20 @@ in
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
-    environment.gnome.excludePackages = (
-      with pkgs;
-      [
-        cheese
-        epiphany
-        geary
-        gedit
-        gnome-characters
-        gnome-console
-        gnome-maps
-        gnome-music
-        gnome-photos
-        gnome-tour
-        gnome-weather
-        simple-scan
-        totem
-      ]
-    );
+    environment.gnome.excludePackages = with pkgs; [
+      cheese
+      epiphany
+      geary
+      gedit
+      gnome-characters
+      gnome-console
+      gnome-maps
+      gnome-music
+      gnome-photos
+      gnome-tour
+      gnome-weather
+      simple-scan
+      totem
+    ];
   };
 }
