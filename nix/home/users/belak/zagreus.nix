c86fb9f682a1be6ac71a6c8b45911198f87b21ec
Kaleb Elwert <belak@coded.io>
2025-09-21 22:57:19 -0700

    nix: various zagreus tweaks

--- nix/home/users/belak/zagreus.nix
+++ nix/home/users/belak/zagreus.nix
@@ -9,7 +9,21 @@
     emacs.enable = true;
     gnome.enable = true;
     gui.enable = true;
+    vscode.enable = true;
   };
 
-  home.packages = with pkgs; [ ];
+  home.packages = with pkgs; [
+    eww
+    foot
+    fuzzel
+    kitty
+    niri
+    #niriswitcher
+    river
+    senpai
+    swaylock
+    uwsm
+    wofi
+    xwayland-satellite
+  ];
 }
