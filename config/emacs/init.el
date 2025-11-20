91d1a2d07bba1fcd7f516cb08551989fad6c361c
Kaleb Elwert <belak@coded.io>
2025-11-19 20:44:23 -0800

    emacs: minor comment updates

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -11,13 +11,13 @@
       (debug-on-quit t)
       ;; Every require/load looks at this, so removing it gets us a small
       ;; performance improvement. However we do want it set after loading
-      ;; everything, so we use let so this will return to normal after this
-      ;; block.
+      ;; everything, so we use `let' so these variables will return to normal
+      ;; after this block.
       (file-name-handler-alist nil))
 
   (require 'belak-core)                 ; low level setup
   (require 'belak-ui)                   ; make things pretty... well, prettier
-  (require 'belak-editor)               ; load the text editing portion of this OS
+  (require 'belak-editor)               ; the text editing portion of this OS
   ;;(require 'belak-evil)                 ; the text-editor program
 
   (when IS-MAC
@@ -27,7 +27,7 @@
   (require 'belak-lang-c)               ; always a classic
   (require 'belak-lang-elisp)           ; Parens. Parens everywhere.
   (require 'belak-lang-go)              ; "Google Golang"
-  (require 'belak-lang-nix)             ;
+  (require 'belak-lang-nix)             ; declarative and flaky
   (require 'belak-lang-python)          ; not super sexy, but super stable
   (require 'belak-lang-rust)            ; memory safety at the cost of sanity
   (require 'belak-lang-web)             ; the tubes
