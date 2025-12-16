19f8853d2490babf659b343c6b7ac27b94d11c2b
Kaleb Elwert <belak@coded.io>
2025-12-16 14:51:00 -0800

    nix: replace custom home-manager trampolines with built-in copyApps

--- nix/home/modules/darwin.nix
+++ nix/home/modules/darwin.nix
@@ -12,22 +12,10 @@
       mkalias
     ];
 
-    # This is a hack adapted from https://github.com/LnL7/nix-darwin/issues/214#issuecomment-2048491929
-    # which creates aliases for home-manager applications, allowing them
-    # to be found by Alfred and spotlight, as long as com.apple.alias-file
-    # is specified under additional file types.
-    home.activation = {
-      makeTrampolines = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        app_folder="${config.home.homeDirectory}/Applications/Home Manager Trampolines"
-        rm -rf "$app_folder"
-        mkdir -p "$app_folder"
-        for app in $genProfilePath/home-path/Applications/*.app; do
-          app_target="$app_folder/$(basename "$app")"
-          real_app=$(readlink -f "$app")
-          echo "mkalias \"$real_app\" \"$app_target\"" >&2
-          $DRY_RUN_CMD ${pkgs.mkalias}/bin/mkalias "$real_app" "$app_target"
-        done
-      '';
-    };
+    # This makes applications show up under spotlight, without needing to manage
+    # it manually. It's enabled by default with home-manager state version >=
+    # 25.11, but many of my setups are older than that.
+    targets.darwin.copyApps.enable = true;
+    targets.darwin.linkApps.enable = false;
   };
 }
