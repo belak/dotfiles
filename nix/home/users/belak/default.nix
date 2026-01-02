244b5376502df876cc79e4d4ef5502016da50398
Kaleb Elwert <belak@coded.io>
2026-01-02 03:01:17 -0800

    nix: allow home.stateVersion to be overridden

--- nix/home/users/belak/default.nix
+++ nix/home/users/belak/default.nix
@@ -11,5 +11,5 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = lib.mkDefault "23.11"; # Please read the comment before changing.
 }
