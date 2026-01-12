11b5df1414b3bf33694315e0d94a545322c142d9
Kaleb Elwert <belak@coded.io>
2026-01-11 23:24:21 -0800

    nix: fix beatrix disk

--- nix/nixos/hosts/beatrix/disko-config.nix
+++ nix/nixos/hosts/beatrix/disko-config.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/sda";
+        device = "/dev/nvme0n1";
         type = "disk";
         content = {
           type = "gpt";
