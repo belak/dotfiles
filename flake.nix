256592ab1e4c98e9904c8c1cf9bee234340ef34f
Kaleb Elwert <kaleb.elwert@datadoghq.com>
2026-01-15 10:56:39 -0800

    nix: migrate to flake-parts, expose all packages

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -57,240 +62,248 @@
   outputs =
     inputs@{
       self,
-      agenix,
-      nixpkgs-unstable,
+      nixpkgs,
+      flake-parts,
       ...
     }:
     let
-      lib = import ./nix/lib.nix inputs;
+      myLib = import ./nix/lib.nix inputs;
     in
-    {
-      inherit lib;
 
-      overlays = import ./nix/overlays.nix inputs;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-      darwinModules.default = import ./nix/darwin/modules;
-      homeModules.default = import ./nix/home/modules;
-      nixosModules.default = import ./nix/nixos/modules;
+      flake = {
+        lib = myLib;
 
-      # There are a number of different formatters available: nixfmt, alejandra,
-      # and nixfmt-rfc-style. As rfc-style is the "up-and-coming" format, we use
-      # that rather than stock nixfmt.
-      formatter = lib.forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs-unstable.legacyPackages.${system};
-        in
-        pkgs.treefmt.withConfig {
-          runtimeInputs = [ pkgs.nixfmt-rfc-style ];
+        darwinModules.default = import ./nix/darwin/modules;
+        homeModules.default = import ./nix/home/modules;
+        nixosModules.default = import ./nix/nixos/modules;
 
-          settings = {
-            # Log level for files treefmt won't format
-            on-unmatched = "info";
+        overlays = import ./nix/overlays.nix inputs;
 
-            # Configure nixfmt for .nix files
-            formatter.nixfmt = {
-              command = "nixfmt";
-              includes = [ "*.nix" ];
-            };
+        darwinConfigurations = {
+          "baku" = myLib.mkDarwinSystem {
+            modules = [ ./nix/darwin/hosts/baku.nix ];
           };
-        }
-      );
 
-      devShells = lib.forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs-unstable.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              agenix.packages.${system}.agenix
-              deploy-rs
-              nixos-anywhere
+          "melinoe" = myLib.mkDarwinSystem {
+            modules = [
+              ./nix/darwin/hosts/melinoe.nix
             ];
           };
-        }
-      );
-
-      darwinConfigurations = {
-        "baku" = lib.mkDarwinSystem {
-          modules = [ ./nix/darwin/hosts/baku.nix ];
         };
 
-        "melinoe" = lib.mkDarwinSystem {
-          modules = [
-            ./nix/darwin/hosts/melinoe.nix
-          ];
+        nixosConfigurations = {
+          # Thinkpad T14 (i5)
+          "beatrix" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/beatrix
+              ./nix/nixos/users/belak
+            ];
+          };
+
+          # ThinkCentre M93p
+          "eiko" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/eiko
+              ./nix/nixos/users/belak
+            ];
+          };
+
+          # Beelink Mini S12 Pro
+          "freya" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/freya
+              ./nix/nixos/users/belak
+            ];
+          };
+
+          # MacBook UTM Instance
+          "hades" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/hades
+              ./nix/nixos/users/belak
+            ];
+          };
+
+          # ThinkPad X13s Gen 1
+          "quina" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/quina
+              ./nix/nixos/users/belak
+            ];
+          };
+
+          # ThinkPad T460
+          "zidane" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/zidane
+              ./nix/nixos/users/belak
+            ];
+          };
+
+          # Primary Desktop
+          "zorn" = myLib.mkNixosSystem {
+            modules = [
+              ./nix/nixos/hosts/zorn
+              ./nix/nixos/users/belak
+            ];
+          };
+        };
+
+        # There are some things nixos and nix-darwin can't provide; for everything
+        # else there's home-manager.
+        #
+        # It's better for these to be host-specific configs, so we can still have
+        # host-specific configuration, but we provide a default "belak" fallback
+        # for the most common cases.
+        homeConfigurations = {
+          "belak@baku" = myLib.mkHome {
+            system = "x86_64-darwin";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/baku.nix
+            ];
+          };
+          "belak@beatrix" = myLib.mkHome {
+            system = "x86_64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/beatrix.nix
+            ];
+          };
+          "belak@eiko" = myLib.mkHome {
+            system = "x86_64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/eiko.nix
+            ];
+          };
+          "belak@freya" = myLib.mkHome {
+            system = "x86_64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/freya.nix
+            ];
+          };
+          "belak@hades" = myLib.mkHome {
+            system = "aarch64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/hades.nix
+            ];
+          };
+          "belak@quina" = myLib.mkHome {
+            system = "aarch64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/quina.nix
+            ];
+          };
+          "belak@melinoe" = myLib.mkHome {
+            system = "aarch64-darwin";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/melinoe.nix
+            ];
+          };
+          "belak@zidane" = myLib.mkHome {
+            system = "x86_64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/zidane.nix
+            ];
+          };
+          "belak@zorn" = myLib.mkHome {
+            system = "x86_64-linux";
+            modules = [
+              ./nix/home/users/belak
+              ./nix/home/users/belak/zorn.nix
+            ];
+          };
+        };
+
+        deploy.nodes = {
+          eiko = {
+            hostname = "eiko.elwert.dev";
+            profilesOrder = [
+              "belak"
+              "system"
+            ];
+            profiles.belak = myLib.mkHomeDeploy self.homeConfigurations."belak@eiko";
+            profiles.system = myLib.mkNixosDeploy self.nixosConfigurations.eiko;
+          };
+
+          freya = {
+            hostname = "freya.elwert.dev";
+            profilesOrder = [
+              "belak"
+              "system"
+            ];
+            profiles.belak = myLib.mkHomeDeploy self.homeConfigurations."belak@freya";
+            profiles.system = myLib.mkNixosDeploy self.nixosConfigurations.freya;
+          };
+
+          zidane = {
+            hostname = "zidane.elwert.dev";
+            profilesOrder = [
+              "belak"
+              "system"
+            ];
+            profiles.belak = myLib.mkHomeDeploy self.homeConfigurations."belak@zidane";
+            profiles.system = myLib.mkNixosDeploy self.nixosConfigurations.zidane;
+          };
         };
       };
 
-      nixosConfigurations = {
-        # Thinkpad T14 (i5)
-        "beatrix" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/beatrix
-            ./nix/nixos/users/belak
-          ];
-        };
+      perSystem =
+        {
+          pkgs,
+          system,
+          config,
+          lib,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = builtins.attrValues self.overlays;
+            config = { };
+          };
 
-        # ThinkCentre M93p
-        "eiko" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/eiko
-            ./nix/nixos/users/belak
-          ];
-        };
+          # There are a number of different formatters available: nixfmt, alejandra,
+          # and nixfmt-rfc-style. As rfc-style is the "up-and-coming" format, we use
+          # that rather than stock nixfmt.
+          formatter = pkgs.treefmt.withConfig {
+            runtimeInputs = [ pkgs.nixfmt-rfc-style ];
 
-        # Beelink Mini S12 Pro
-        "freya" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/freya
-            ./nix/nixos/users/belak
-          ];
-        };
+            settings = {
+              # Log level for files treefmt won't format
+              on-unmatched = "info";
 
-        # MacBook UTM Instance
-        "hades" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/hades
-            ./nix/nixos/users/belak
-          ];
-        };
+              # Configure nixfmt for .nix files
+              formatter.nixfmt = {
+                command = "nixfmt";
+                includes = [ "*.nix" ];
+              };
+            };
+          };
 
-        # ThinkPad X13s Gen 1
-        "quina" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/quina
-            ./nix/nixos/users/belak
-          ];
-        };
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                inputs.agenix.packages.${system}.agenix
+                deploy-rs
+                nixos-anywhere
+              ];
+            };
+          };
 
-        # ThinkPad T460
-        "zidane" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/zidane
-            ./nix/nixos/users/belak
-          ];
+          packages = lib.packagesFromDirectoryRecursive {
+            inherit (pkgs) callPackage;
+            directory = ./nix/pkgs;
+          };
         };
-
-        # Primary Desktop
-        "zorn" = lib.mkNixosSystem {
-          modules = [
-            ./nix/nixos/hosts/zorn
-            ./nix/nixos/users/belak
-          ];
-        };
-      };
-
-      # There are some things nixos and nix-darwin can't provide; for everything
-      # else there's home-manager.
-      #
-      # It's better for these to be host-specific configs, so we can still have
-      # host-specific configuration, but we provide a default "belak" fallback
-      # for the most common cases.
-      homeConfigurations = {
-        "belak@baku" = lib.mkHome {
-          system = "x86_64-darwin";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/baku.nix
-          ];
-        };
-        "belak@beatrix" = lib.mkHome {
-          system = "x86_64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/beatrix.nix
-          ];
-        };
-        "belak@eiko" = lib.mkHome {
-          system = "x86_64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/eiko.nix
-          ];
-        };
-        "belak@freya" = lib.mkHome {
-          system = "x86_64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/freya.nix
-          ];
-        };
-        "belak@hades" = lib.mkHome {
-          system = "aarch64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/hades.nix
-          ];
-        };
-        "belak@quina" = lib.mkHome {
-          system = "aarch64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/quina.nix
-          ];
-        };
-        "belak@melinoe" = lib.mkHome {
-          system = "aarch64-darwin";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/melinoe.nix
-          ];
-        };
-        "belak@zidane" = lib.mkHome {
-          system = "x86_64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/zidane.nix
-          ];
-        };
-        "belak@zorn" = lib.mkHome {
-          system = "x86_64-linux";
-          modules = [
-            ./nix/home/users/belak
-            ./nix/home/users/belak/zorn.nix
-          ];
-        };
-      };
-
-      deploy.nodes = {
-        eiko = {
-          hostname = "eiko.elwert.dev";
-          profilesOrder = [
-            "belak"
-            "system"
-          ];
-          profiles.belak = lib.mkHomeDeploy self.homeConfigurations."belak@eiko";
-          profiles.system = lib.mkNixosDeploy self.nixosConfigurations.eiko;
-        };
-
-        freya = {
-          hostname = "freya.elwert.dev";
-          profilesOrder = [
-            "belak"
-            "system"
-          ];
-          profiles.belak = lib.mkHomeDeploy self.homeConfigurations."belak@freya";
-          profiles.system = lib.mkNixosDeploy self.nixosConfigurations.freya;
-        };
-
-        zidane = {
-          hostname = "zidane.elwert.dev";
-          profilesOrder = [
-            "belak"
-            "system"
-          ];
-          profiles.belak = lib.mkHomeDeploy self.homeConfigurations."belak@zidane";
-          profiles.system = lib.mkNixosDeploy self.nixosConfigurations.zidane;
-        };
-      };
-
-      packages = lib.forAllSystems (system: {
-        install-iso-minimal = import ./nix/pkgs/install-iso-minimal.nix inputs;
-        rpi-bootstrap-image = import ./nix/pkgs/rpi-bootstrap-image.nix inputs;
-        x13s-install-iso-minimal = import ./nix/pkgs/rpi-bootstrap-image.nix inputs;
-      });
     };
 }
