e15d8775ae7e4fb81b214cf6d1e33e7efff3e70b
Kaleb Elwert <kaleb.elwert@datadoghq.com>
2026-01-30 01:41:19 -0800

    fish: tweak PATH order

--- config/fish/conf.d/01-path.fish
+++ config/fish/conf.d/01-path.fish
@@ -5,13 +5,13 @@ fish_add_path --prepend \
     $HOME/bin
 
 # Application binaries
-fish_add_path --prepend \
+fish_add_path \
     /Applications/Ghostty.app/Contents/MacOS \
     /Applications/Postgres.app/Contents/Versions/latest/bin \
     "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 # Development tools
-fish_add_path --prepend \
+fish_add_path \
     $GOPATH/bin \
     $VOLTA_HOME/bin \
     $HOME/.cargo/bin \
