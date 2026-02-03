2b1b9acc2a96e18ca56e36d500f315e67420ce1e
Kaleb Elwert <belak@coded.io>
2026-02-03 11:01:01 -0800

    Revert "nix: auto-import modules rather than relying on manual imports"
    This reverts commit 68660a1abfc1328c41e99271385f7826c50ad884.

---
+++ nix/home/modules/default.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ./common.nix
+    ./nixpkgs.nix
+
+    # Platforms
+    ./darwin.nix
+    ./linux.nix
+
+    # Feature Modules
+    ./dev.nix
+    ./dotfiles.nix
+    ./emacs.nix
+    ./gnome.nix
+    ./gui.nix
+    ./vscode.nix
+  ];
+}
