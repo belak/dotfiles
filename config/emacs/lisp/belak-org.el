57df69c500384fba7c3ba12ca1566bd7dbecb5b6
Kaleb Elwert <belak@coded.io>
2026-02-16 15:13:08 -0800

    emacs: experiment with a more opinionated org-mode setup

--- config/emacs/lisp/belak-org.el
+++ config/emacs/lisp/belak-org.el
@@ -12,20 +12,69 @@
 ;; to README.org again.
 
 ;;
+;;; Variables
+
+(defvar belak-org-directory
+  (expand-file-name "~/Documents/org")
+  "Base directory for all org files.")
+
+(defvar belak-org-context-filter
+  (if IS-WORK 'work 'home)
+  "Current org context filter: 'work, 'home, or nil.
+When nil, no filter is applied and all directories are included in agenda.")
+
+
+;;
 ;;; Functions
 
-;; Initially from Sacha Chua's Emacs config
-;; TODO: bind to something?
-(defun belak-org-insert-heading-for-next-day ()
-  "Insert a same-level heading for the following day."
+(defun belak/org-update-agenda-files ()
+  "Update org-agenda-files based on current context filter."
+  (setq org-agenda-files
+        (pcase belak-org-context-filter
+          ('work  (list (expand-file-name "work" belak-org-directory)))
+          ('home  (list (expand-file-name "home" belak-org-directory)))
+          (_      (list belak-org-directory)))))
+
+(defun belak/org-toggle-context ()
+  "Toggle between work, home, and nil contexts.
+Updates agenda files and refreshes agenda if open."
   (interactive)
-  (let ((new-date
-         (seconds-to-time
-          (+ 86400.0
-             (float-time
-              (org-read-date nil 'to-time (elt (org-heading-components) 4)))))))
-    (org-insert-heading-after-current)
-    (insert (format-time-string "%Y-%m-%d\n\n" new-date))))
+  (setq belak-org-context-filter
+        (pcase belak-org-context-filter
+          ('work 'home)
+          ('home nil)
+          (_     'work)))
+  (belak/org-update-agenda-files)
+  (when (derived-mode-p 'org-agenda-mode)
+    (org-agenda-redo))
+  (message "Org context: %s" (or belak-org-context-filter 'all)))
+
+(defun belak/org-context-dir ()
+  "Return the context directory based on current context filter.
+When context is nil, uses IS-WORK to determine directory."
+  (pcase belak-org-context-filter
+    ('work "work")
+    ('home "home")
+    (_     (if IS-WORK "work" "home"))))
+
+(defun belak/org-context-file (subpath)
+  "Return path to file at SUBPATH within current context directory."
+  (expand-file-name
+   (format "%s/%s" (belak/org-context-dir) subpath)
+   belak-org-directory))
+
+(defun belak/org-current-weekly-file ()
+  "Return path to current week's org file based on context filter."
+  (belak/org-context-file
+   (format-time-string "weekly/%Y-W%V.org")))
+
+(defun belak/org-current-meetings-file ()
+  "Return path to meetings file based on context filter."
+  (belak/org-context-file "meetings.org"))
+
+(defun belak/org-current-inbox-file ()
+  "Return path to inbox file based on context filter."
+  (belak/org-context-file "inbox.org"))
 
 
 ;;
@@ -38,11 +87,15 @@
          ("C-c c" . org-capture)
          ("C-c l" . org-store-link)
          ("C-c j" . org-clock-goto)
+         ("C-c t" . belak/org-toggle-context)
          (:map org-mode-map
                ("C-c C-r" . org-refile)
                ("C-c v"   . org-show-todo-tree)))
   :config
   (setq
+   ;; Base directory
+   org-directory belak-org-directory
+
    ;; Allow using shift-select like in other buffers.
    org-support-shift-select t
 
@@ -70,8 +123,7 @@
 
    ;; Tweak our task keywords
    org-todo-keywords
-   '((sequence "TODO(t)" "WIP(p)" "WAITING(w)" "SOMEDAY(.)" "|" "DONE(d)" "CANCELLED(c)")
-     (sequence "TRY" "LEARN" "|" "COMPLETE(x)"))
+   '((sequence "TODO(t)" "WIP(p)" "|" "DONE(d)" "CANCELLED(c)"))
 
    ;; Open source blocks in the same window.
    org-src-window-setup 'current-window
@@ -91,43 +143,69 @@
 
    org-agenda-dim-blocked-tasks t
 
-   ;; TODO: org-log-into-drawer, org-clock-into-drawer
-   ;; TODO: look at Sacha's effort property
-   ;; TODO: look at Sacha's agenda-contexts
-   ;; TODO: look at Sacha's org-agenda-done and org-agenda-mark-done-and-add-followup
-   ;; TODO: look at Sacha's weekly and monthly reviews
-   ;; TODO: look at org-move-line-to-end-of-list
-   ;; TODO: look at org-attach for board game files
-   ;; TODO: look at org-agenda
-   ;; TODO: look at org-clock
-
    org-babel-load-languages '((emacs-lisp . t)
                               (calc . t)
                               (python . t)
                               (shell . t)
                               (ruby . t))
 
-   ;; TODO: maybe replace calc with literate-calc and set up org-src-lang-modes
-   ;; TODO: look at Sacha's org-sort-list-in-custom-order
-   ;; TODO: look at some of the Emacs news feeds in Sacha's config
-
    org-use-effective-time t
    org-extend-today-until 5
 
    ;; Show headlines but not content by default.
    org-startup-folded 'content
-   )
 
-  ;;(add-function :after after-focus-change-function 'org-save-all-org-buffers)
-  )
+   ;; Capture templates
+   org-capture-templates
+   '(("t" "Task" entry
+      (file belak/org-current-weekly-file)
+      "* TODO %?\n"
+      :empty-lines 1)
 
-(use-package org-contrib
-  :after org)
+     ("m" "Meeting" entry
+      (file+headline belak/org-current-meetings-file "Meetings")
+      "* %<%Y-%m-%d %H:%M> - %?\n** Attendees\n- \n\n** Notes\n\n** Action Items\n- [ ] \n"
+      :empty-lines 1)
 
-(use-package org-roam
+     ("n" "Note" entry
+      (file belak/org-current-inbox-file)
+      "* %?\n%U\n"
+      :empty-lines 1)
+
+     ("j" "Journal" entry
+      (file+olp+datetree (lambda () (expand-file-name "home/journal.org" belak-org-directory)))
+      "* %<%H:%M>\n%?\n"
+      :empty-lines 1)))
+
+  ;; Initialize agenda files based on context
+  (belak/org-update-agenda-files))
+
+(use-package org-ql
+  :demand t
   :after org
-  :blackout
-  :hook (after-init . org-roam-mode))
+  :config
+  ;; Explicitly require org-ql-search to ensure org-ql-block is available
+  (require 'org-ql-search)
+
+  (setq org-agenda-custom-commands
+        '(("n" "Next Actions"
+           ((org-ql-block '(and (todo "TODO")
+                                (not (scheduled))
+                                (not (deadline)))
+                          ((org-ql-block-header "Next Actions")
+                           (org-agenda-sorting-strategy '(priority-down))))))
+
+          ("r" "Weekly Review"
+           ((org-ql-block '(todo "TODO" "WIP")
+                          ((org-ql-block-header "Open Tasks")))
+            (org-ql-block '(and (todo "DONE")
+                                (closed :from -7))
+                          ((org-ql-block-header "Completed Last 7 Days")))))
+
+          ("t" "All Tasks"
+           ((org-ql-block '(todo "TODO" "WIP")
+                          ((org-ql-block-header "All Tasks")
+                           (org-agenda-sorting-strategy '(priority-down category-keep)))))))))
 
 (provide 'belak-org)
 ;;; belak-org.el ends here
