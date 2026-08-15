a9f60298ee117f45577ddbca257d776cd50e0b0a
Kaleb Elwert <belak@coded.io>
2026-08-15 08:30:34 -0700

    nix: move the public keys into keys.nix

---
+++ secrets/keys.nix
@@ -0,0 +1,41 @@
+# Public SSH keys, used as both agenix recipients and the root authorizedKeys in
+# the server module.
+rec {
+  user-belak-beatrix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6gzteIYFVEhBmz+bA8SqIPKuYXk0HjLJJnw9dDTZwd";
+  user-belak-hades = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK69hivmEYShurplMVlBfRanBi4St0pbnbRXSP0n7Qnm";
+  user-belak-melinoe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMzuXboQDv2VCig0+A780O0+sKs1euw+3OafnRA6z14P";
+  user-belak-quina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjxw57nR3VIhpVt9zYipzLqZ0ecHhDBjyP8dNhxL5mP";
+  user-belak-zorn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvdWkVKcV087KDa9e2fdaubwW8SztSo+k+lYaeEKILC";
+
+  user-belak-work = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUSx9TTTHUq4GOkeBU4Ga03QombEBiZLqqa8KIqnnUy";
+
+  users = [
+    user-belak-beatrix
+    user-belak-hades
+    user-belak-melinoe
+    user-belak-quina
+    user-belak-zorn
+
+    user-belak-work
+  ];
+
+  system-baku = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUhTjBux/Puqhpa4TgphZYsXIClhMWF0iOTZugc0k6a";
+  system-beatrix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8DedpQ6Q+OqlMeiQydzu89Q2xIGGAIIl4+tyXy584v";
+  system-freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDU1rGovd901nTi60c/WTDtTrkWSJ8V2lDMJr6MusKWS";
+  system-hades = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB84SBcMThfhBWlPiW1ySels6Ri17TDoDSjuuoX4tfF";
+  system-quina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZ06jS8rephEg8IZgqkwBJ7QRPH7Osh+HE0LU6q2YvC";
+  system-vivi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDx5Y7VvA9CUdrsiVpNbRufBdJdvJZEfRQXIGnPgqynH";
+  system-zidane = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8pkaoi6ASLpjFP+9v/frMX6wAiWrM3LTMvkdnU8Rd0";
+  system-zorn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIl/rte7VsiKLVGhRLz3eSYh4Ln3YO9h/CQEOrib4xKe";
+
+  systems = [
+    system-baku
+    system-beatrix
+    system-freya
+    system-hades
+    system-quina
+    system-vivi
+    system-zidane
+    system-zorn
+  ];
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,56 +1,21 @@
 let
-  user-belak-beatrix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6gzteIYFVEhBmz+bA8SqIPKuYXk0HjLJJnw9dDTZwd";
-  user-belak-hades = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK69hivmEYShurplMVlBfRanBi4St0pbnbRXSP0n7Qnm";
-  user-belak-melinoe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMzuXboQDv2VCig0+A780O0+sKs1euw+3OafnRA6z14P";
-  user-belak-quina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjxw57nR3VIhpVt9zYipzLqZ0ecHhDBjyP8dNhxL5mP";
-  user-belak-zorn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvdWkVKcV087KDa9e2fdaubwW8SztSo+k+lYaeEKILC";
+  keys = import ./keys.nix;
+  inherit (keys) users systems;
 
-  user-belak-work = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUSx9TTTHUq4GOkeBU4Ga03QombEBiZLqqa8KIqnnUy";
-
-  users = [
-    user-belak-beatrix
-    user-belak-hades
-    user-belak-melinoe
-    user-belak-quina
-    user-belak-zorn
-
-    user-belak-work
-  ];
-
-  system-baku = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUhTjBux/Puqhpa4TgphZYsXIClhMWF0iOTZugc0k6a";
-  system-beatrix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8DedpQ6Q+OqlMeiQydzu89Q2xIGGAIIl4+tyXy584v";
-  system-freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDU1rGovd901nTi60c/WTDtTrkWSJ8V2lDMJr6MusKWS";
-  system-hades = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB84SBcMThfhBWlPiW1ySels6Ri17TDoDSjuuoX4tfF";
-  system-quina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZ06jS8rephEg8IZgqkwBJ7QRPH7Osh+HE0LU6q2YvC";
-  system-vivi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDx5Y7VvA9CUdrsiVpNbRufBdJdvJZEfRQXIGnPgqynH";
-  system-zidane = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8pkaoi6ASLpjFP+9v/frMX6wAiWrM3LTMvkdnU8Rd0";
-  system-zorn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIl/rte7VsiKLVGhRLz3eSYh4Ln3YO9h/CQEOrib4xKe";
-
-  systems = [
-    system-baku
-    system-beatrix
-    system-freya
-    system-hades
-    system-quina
-    system-vivi
-    system-zidane
-    system-zorn
-  ];
-
-  service-atticd = [ system-quina ];
-  service-forgejo = [ system-baku ];
-  service-immich = [ system-freya ];
-  service-kavita = [ system-freya ];
-  service-miniflux = [ system-freya ];
-  service-pocket-id = [ system-zidane ];
-  service-syncthing = [ system-freya ];
-  service-woodpecker = [ system-baku ];
-  service-woodpecker-agent = [ system-quina ];
+  service-atticd = [ keys.system-quina ];
+  service-forgejo = [ keys.system-baku ];
+  service-immich = [ keys.system-freya ];
+  service-kavita = [ keys.system-freya ];
+  service-miniflux = [ keys.system-freya ];
+  service-pocket-id = [ keys.system-zidane ];
+  service-syncthing = [ keys.system-freya ];
+  service-woodpecker = [ keys.system-baku ];
+  service-woodpecker-agent = [ keys.system-quina ];
 in
 {
   "atticd-env.age".publicKeys = service-atticd ++ users;
 
-  "acme-cloudflare-env.age".publicKeys = users ++ [ system-zidane ];
+  "acme-cloudflare-env.age".publicKeys = users ++ [ keys.system-zidane ];
   "belak-password.age".publicKeys = users ++ systems;
 
   "forgejo-oidc-client-id.age".publicKeys = service-forgejo ++ users;
