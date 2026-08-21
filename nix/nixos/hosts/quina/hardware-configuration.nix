606f49618bf3417a2cfd9b24e65c68820e27e4f0
Kaleb Elwert <belak@coded.io>
2026-08-21 01:10:27 -0700

    nix(quina): switch to networkmanager

--- nix/nixos/hosts/quina/hardware-configuration.nix
+++ nix/nixos/hosts/quina/hardware-configuration.nix
@@ -74,9 +74,13 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  #networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlP6p1s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
+
+  # This box was converted back into a personal laptop, so make sure it's
+  # running something easy to use and flexible.
+  networking.networkmanager.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
