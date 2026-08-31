550ab1c9801412899c20cef4321048b0892a4b12
Kaleb Elwert <belak@coded.io>
2026-08-31 10:20:29 -0700

    nix(emacs): switch to unstable.emacs rather than building from an overlay

--- nix/home/modules/emacs.nix
+++ nix/home/modules/emacs.nix
@@ -20,7 +20,7 @@ in
         #
         # XXX: unfortunately there are some issues with the pgtk build on macOS,
         # so we just use the normal emacs build there.
-        myEmacs = if pkgs.stdenv.isDarwin then pkgs.emacs-unstable else pkgs.emacs-unstable-pgtk;
+        myEmacs = if pkgs.stdenv.isDarwin then pkgs.unstable.emacs else pkgs.unstable.emacs-pgtk;
         emacsWithPackages = (emacsPackagesFor myEmacs).emacsWithPackages;
       in
       [
