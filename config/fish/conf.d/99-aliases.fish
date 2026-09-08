94b35ff87849ffe237cb75a74b1f7eefcf4017f0
Kaleb Elwert <belak@coded.io>
2026-09-07 23:27:32 -0700

    fish: add a few useful aliases

--- config/fish/conf.d/99-aliases.fish
+++ config/fish/conf.d/99-aliases.fish
@@ -4,3 +4,7 @@ alias vim="nvim"
 # Utility aliases
 alias json="python -mjson.tool"
 alias k="kubectl"
+alias t="task"
+
+alias tolower="tr '[:upper:]' '[:lower:]'"
+alias toupper="tr '[:lower:]' '[:upper:]'"
